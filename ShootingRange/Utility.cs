dc066de9d523dd14f35a7ced06c5c002e6dca55a
Nektarios Gouvousis <gouvousisnektarios@gmail.com>
2023-03-12 16:52:09 +0200

    ?

--- ShootingRange/Utility.cs
+++ ShootingRange/Utility.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace ShootingRange
 {
@@ -13,7 +14,17 @@ namespace ShootingRange
             string tk, string region, string stathero, string kinhto,
             string email, string mhtrwo, string sullogos)
         {
-            
+            string reg_date = "\\d{2}-\\d{2}-\\d{4}|\\d{2}\\/\\d{2}\\/\\d{4}";
+            if (Regex.Match(bday, reg_date).Success)
+            {
+                Console.WriteLine("MPRAVO");
+            }
+            else
+            {
+                Console.WriteLine("MPRAVO MALAKA");
+            }
+            Console.WriteLine("Ladia");
+            return 0;
         }
     }
 }
